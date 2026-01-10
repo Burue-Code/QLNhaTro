93fa3a7bebb6c10aa7d397ddaba42ea356153c0c
Burue-Code <tatinh211@gmail.com>
2026-01-10 19:31:20 +0700

    chỉnh lại form chính

--- QLNhaTro/src/com/nctu/quanlynhatro/view/phu_phi/PhuPhiView.java
+++ QLNhaTro/src/com/nctu/quanlynhatro/view/phu_phi/PhuPhiView.java
@@ -1,5 +1,20 @@
 package com.nctu.quanlynhatro.view.phu_phi;
 
-public class PhuPhiView {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PhuPhiView extends JPanel{
+	
+	public PhuPhiView() {
+		setLayout(new BorderLayout());
+        setBackground(new Color(230, 240, 255)); // Màu nhẹ
+        JLabel lbl = new JLabel("TRANG CHỦ DASHBOARD", SwingConstants.CENTER);
+        lbl.setFont(new Font("Arial", Font.BOLD, 20));
+        add(lbl, BorderLayout.CENTER);
+	}
 }
--- QLNhaTro/src/com/nctu/quanlynhatro/view/thong_ke/ThongKeDoanhThuView.java
+++ QLNhaTro/src/com/nctu/quanlynhatro/view/thong_ke/ThongKeDoanhThuView.java
@@ -1,5 +1,19 @@
 package com.nctu.quanlynhatro.view.thong_ke;
 
-public class ThongKeDoanhThuView {
+import java.awt.Color;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ThongKeDoanhThuView extends JPanel{
+	public ThongKeDoanhThuView() {
+        setLayout(new FlowLayout());
+        setBackground(new Color(255, 230, 230)); // Màu nhẹ khác
+        add(new JLabel("Quản lý danh sách sinh viên"));
+        add(new JTextField(20));
+        add(new JButton("Tìm kiếm"));
+    }
 }
